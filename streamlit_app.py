aeffbc25b97f1bdfc0b652c8aead0653f79e73c2
Mohammed Husamuddin <uni.husam@gmail.com>
2023-12-18 07:57:50 +0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,24 +25,30 @@ X_test = sc.transform(X_test)
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 
-#App Settings and Title
+# App Settings and Title
 st.title('Heart Rate Prediction using Machine Learning')
+
+# Input explanations
+explanations = {
+    'age': 'The person’s age in years',
+    'sex': 'The person’s sex (1 = male, 0 = female)',
+    'cp': 'Chest pain type:\n0: asymptomatic\n1: atypical angina\n2: non-anginal pain\n3: typical angina',
+    'trestbps': 'The person’s resting blood pressure (mm Hg on admission to the hospital)',
+    'chol': 'The person’s cholesterol measurement in mg/dl',
+    'fbs': 'The person’s fasting blood sugar (> 120 mg/dl, 1 = true; 0 = false)',
+    'restecg': 'Resting electrocardiographic results:\n0: showing probable or definite left ventricular hypertrophy by Estes’ criteria\n1: normal\n2: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)',
+    'thalach': 'The person’s maximum heart rate achieved',
+    'exang': 'Exercise-induced angina (1 = yes; 0 = no)',
+    'oldpeak': 'ST depression induced by exercise relative to rest',
+    'slope': 'The slope of the peak exercise ST segment:\n0: downsloping\n1: flat\n2: upsloping',
+    'ca': 'The number of major vessels (0–3)',
+    'thal': 'A blood disorder called thalassemia\n0: NULL (dropped from the dataset previously)\n1: fixed defect (no blood flow in some part of the heart)\n2: normal blood flow\n3: reversible defect (a blood flow is observed but it is not normal)'
+}
 
 # Create an interface for the user to input their details
 with st.form(key='user_details'):
-    age = st.slider('Age', 1, 100, 30)
-    sex = st.selectbox('Sex', [0, 1])
-    cp = st.selectbox('Chest Pain Type', [0, 1, 2, 3])
-    trestbps = st.slider('Resting Blood Pressure', 50, 250, 100)
-    chol = st.slider('Serum Cholestoral', 50, 300, 150)
-    fbs = st.selectbox('Fasting Blood Sugar', [0, 1])
-    restecg = st.selectbox('Resting Electrocardiographic Results', [0, 1, 2])
-    thalach = st.slider('Maximum Heart Rate Achieved', 50, 200, 120)
-    exang = st.selectbox('Exercise-Induced Angina', [0, 1])
-    oldpeak = st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 4.0, 0.5)
-    slope = st.selectbox('Slope of the Peak Exercise ST Segment', [0, 1, 2])
-    ca = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', [0, 1, 2, 3])
-    thal = st.selectbox('Thalassemia', [0, 1, 2, 3])
+    for feature in explanations:
+        value = st.slider(explanations[feature], min_value=min(df[feature]), max_value=max(df[feature]), value=df[feature].median()) if feature != 'sex' and feature != 'fbs' and feature != 'exang' else st.selectbox(explanations[feature], options=[0, 1], index=df[feature].mode()[0])
     submit_button = st.form_submit_button(label='Submit')
 
 if submit_button:
