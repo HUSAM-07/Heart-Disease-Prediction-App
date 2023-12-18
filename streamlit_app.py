52688e08a79816a5117afbfa64dc71c4566db19a
Mohammed Husamuddin <uni.husam@gmail.com>
2023-12-18 08:10:02 +0400

    new changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,36 +28,45 @@ knn.fit(X_train, y_train)
 # App Settings and Title
 st.title('Heart Rate Prediction using Machine Learning')
 
-# Input explanations
-explanations = {
-    'age': 'The person’s age in years',
-    'sex': 'The person’s sex (1 = male, 0 = female)',
-    'cp': 'Chest pain type:\n0: asymptomatic\n1: atypical angina\n2: non-anginal pain\n3: typical angina',
-    'trestbps': 'The person’s resting blood pressure (mm Hg on admission to the hospital)',
-    'chol': 'The person’s cholesterol measurement in mg/dl',
-    'fbs': 'The person’s fasting blood sugar (> 120 mg/dl, 1 = true; 0 = false)',
-    'restecg': 'Resting electrocardiographic results:\n0: showing probable or definite left ventricular hypertrophy by Estes’ criteria\n1: normal\n2: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)',
-    'thalach': 'The person’s maximum heart rate achieved',
-    'exang': 'Exercise-induced angina (1 = yes; 0 = no)',
-    'oldpeak': 'ST depression induced by exercise relative to rest',
-    'slope': 'The slope of the peak exercise ST segment:\n0: downsloping\n1: flat\n2: upsloping',
-    'ca': 'The number of major vessels (0–3)',
-    'thal': 'A blood disorder called thalassemia\n0: NULL (dropped from the dataset previously)\n1: fixed defect (no blood flow in some part of the heart)\n2: normal blood flow\n3: reversible defect (a blood flow is observed but it is not normal)'
-}
-
 # Create an interface for the user to input their details
 with st.form(key='user_details'):
-    user_input = {}
-    for feature in explanations:
-        if feature != 'sex' and feature != 'fbs' and feature != 'exang':
-            user_input[feature] = st.slider(explanations[feature], min_value=int(min(df[feature])), max_value=int(max(df[feature])), value=int(df[feature].median()))
-        else:
-            user_input[feature] = st.selectbox(explanations[feature], options=[0, 1], index=df[feature].mode()[0])
+    age = st.slider('Age', 1, 100, 30)
+    
+    # Sex: 0 = female, 1 = male
+    sex = st.selectbox('Sex (0 = female, 1 = male)', [0, 1])
+    
+    # Chest Pain Type
+    cp = st.selectbox('Chest Pain Type', [0, 1, 2, 3], format_func=lambda x: f'{x}: {"Asymptomatic" if x == 0 else "Atypical Angina" if x == 1 else "Non-Anginal Pain" if x == 2 else "Typical Angina"}')
+    
+    trestbps = st.slider('Resting Blood Pressure (mm Hg)', 50, 250, 100)
+    chol = st.slider('Serum Cholestoral (mg/dl)', 50, 300, 150)
+    
+    # Fasting Blood Sugar: 0 = false, 1 = true
+    fbs = st.selectbox('Fasting Blood Sugar (> 120 mg/dl, 1 = true; 0 = false)', [0, 1])
+    
+    # Resting Electrocardiographic Results
+    restecg = st.selectbox('Resting Electrocardiographic Results', [0, 1, 2], format_func=lambda x: f'{x}: {"Probable/Definite LVH" if x == 0 else "Normal" if x == 1 else "ST-T Wave Abnormality"}')
+    
+    thalach = st.slider('Maximum Heart Rate Achieved', 50, 200, 120)
+    
+    # Exercise-Induced Angina: 0 = no, 1 = yes
+    exang = st.selectbox('Exercise-Induced Angina (0 = no, 1 = yes)', [0, 1])
+    
+    oldpeak = st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 4.0, 0.5)
+    
+    # Slope of the Peak Exercise ST Segment: 0 = downsloping, 1 = flat, 2 = upsloping
+    slope = st.selectbox('Slope of the Peak Exercise ST Segment', [0, 1, 2], format_func=lambda x: f'{x}: {"Downsloping" if x == 0 else "Flat" if x == 1 else "Upsloping"}')
+    
+    # Number of Major Vessels Colored by Fluoroscopy
+    ca = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', [0, 1, 2, 3])
+    
+    # Thalassemia
+    thal = st.selectbox('Thalassemia', [0, 1, 2, 3], format_func=lambda x: f'{x}: {"NULL" if x == 0 else "Fixed Defect" if x == 1 else "Normal Blood Flow" if x == 2 else "Reversible Defect"}')
     
     submit_button = st.form_submit_button(label='Submit')
 
 if submit_button:
-    input_data = [[user_input[feature] for feature in explanations]]
+    input_data = [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
     input_data = sc.transform(input_data)
     prediction = knn.predict(input_data)
     if prediction == 1:
